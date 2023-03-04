0af6fc3e3f39eda759e3af8ccd8805033e14806f
shubhamSS9 <shubhamshinde0999@gmail.com>
2023-03-05 00:04:02 +0530

    added UC2_EmployeeDailyWage

--- src/com/Bridglabz/EmployeeWage.java
+++ src/com/Bridglabz/EmployeeWage.java
@@ -2,12 +2,18 @@ package com.Bridglabz;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-    int IS_FULL_TIME = 1;
-    double empCheck = Math.floor(Math.random() * 10) % 2;
-    if(empCheck == IS_FULL_TIME)
-
-        System.out.println("Employee is Present");
-    else
-        System.out.println("Employee is Absent");
+        int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        //variables
+        int empHrs = 0;
+        int empWage = 0;
+        //Computation
+        double empCheck = Math.floor(Math.random()*10)%2;
+        if (empCheck == IS_FULL_TIME)
+            empHrs = 8 ;
+        else
+            empHrs =0;
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("emp wage: "+empWage);
     }
 }
