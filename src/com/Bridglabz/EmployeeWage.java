6f037f6481297a6fe58b4079d00ff5ec897e59d1
shubhamSS9 <shubhamshinde0999@gmail.com>
2023-03-05 00:11:50 +0530

    added UC4_EmployeeWageSwitchCase

--- src/com/Bridglabz/EmployeeWage.java
+++ src/com/Bridglabz/EmployeeWage.java
@@ -3,20 +3,26 @@ package com.Bridglabz;
 public class EmployeeWage {
     public static void main(String[] args) {
         int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
+        int IS_PART_TIME=2;
         int EMP_RATE_PER_HOUR = 20;
         //variables
         int empHrs = 0;
         int empWage = 0;
         //Computation
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_FULL_TIME)
-            empHrs = 8;
-        else if (empCheck == IS_PART_TIME)
-            empHrs = 4;
-        else
-            empHrs = 0;
+        int empCheck =(int) (Math.floor(Math.random()*10)%3);
+        switch(empCheck)
+        {
+            case 1:
+                empHrs=8;
+                break;
+            case 2:
+                empHrs=4;
+                break;
+            case 3:
+                empHrs=0;
+                break;
+        }
         empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("emp wage: " + empWage);
+        System.out.println("emp wage: "+empWage);
     }
 }
